c1027d2ca5b6732f531cb257480bef9a4db95591
Romy Putra <romyputra.545@gmail.com>
2022-09-20 16:57:14 +0700

    Input Existing

---
+++ Controllers/MasterController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XAXO.Controllers
+{
+    [Route("api/Master")]
+    public class MasterController : Controller
+    {
+        [HttpGet]
+        [Route("Test_Swaggers")]
+        public async Task<string> TestSwaggers()
+        {
+            return "Tested";
+        }
+    }
+}
